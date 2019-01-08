81556635a0aedc1c2d8b94bac47d21c39d61c996
Nicolas Ruggiero <nicymo.r@gmail.com>
2019-01-08 10:20:55 -0500

    numbers working

---
+++ calc.js
@@ -0,0 +1,61 @@
+let string = "";
+
+function d1() {
+  var div = document.getElementById("output");
+  string += "1"
+  div.innerHTML = string;
+}
+
+function d2() {
+  var div = document.getElementById("output");
+  string += "2"
+  div.innerHTML = string;
+}
+
+function d3() {
+  var div = document.getElementById("output");
+  string += "3"
+  div.innerHTML = string;
+}
+
+function d4() {
+  var div = document.getElementById("output");
+  string += "4"
+  div.innerHTML = string;
+}
+
+function d5() {
+  var div = document.getElementById("output");
+  string += "5"
+  div.innerHTML = string;
+}
+
+function d6() {
+  var div = document.getElementById("output");
+  string += "6"
+  div.innerHTML = string;
+}
+
+function d7() {
+  var div = document.getElementById("output");
+  string += "7"
+  div.innerHTML = string;
+}
+
+function d8() {
+  var div = document.getElementById("output");
+  string += "8"
+  div.innerHTML = string;
+}
+
+function d9() {
+  var div = document.getElementById("output");
+  string += "9"
+  div.innerHTML = string;
+}
+
+function ac() {
+  var div = document.getElementById("output");
+  string = " "
+  div.innerHTML = string;
+}
