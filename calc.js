49a2eb90b601bb7d4fece5cf6b6d04973a9a6bdb
Nicolas Ruggiero <nicymo.r@gmail.com>
2019-01-14 10:50:15 -0500

    most functions work

--- calc.js
+++ calc.js
@@ -1,56 +1,93 @@
 let string = "";
+var state = 0
 
 function d1() {
+  if (state == 1) {
+    string = "1"
+  } else {
+    string += "1"
+  }
   var div = document.getElementById("output");
-  string += "1"
   div.innerHTML = string;
 }
 
 function d2() {
+  if (state == 1) {
+    string = "2"
+  } else {
+    string += "2"
+  }
   var div = document.getElementById("output");
-  string += "2"
   div.innerHTML = string;
 }
 
 function d3() {
+  if (state == 1) {
+    string = "3"
+  } else {
+    string += "3"
+  }
   var div = document.getElementById("output");
-  string += "3"
   div.innerHTML = string;
 }
 
 function d4() {
+  if (state == 1) {
+    string = "4"
+  } else {
+    string += "4"
+  }
   var div = document.getElementById("output");
-  string += "4"
   div.innerHTML = string;
 }
 
 function d5() {
+  if (state == 1) {
+    string = "5"
+  } else {
+    string += "5"
+  }
   var div = document.getElementById("output");
-  string += "5"
   div.innerHTML = string;
 }
 
 function d6() {
+  if (state == 1) {
+    string = "6"
+  } else {
+    string += "6"
+  }
   var div = document.getElementById("output");
-  string += "6"
   div.innerHTML = string;
 }
 
 function d7() {
+  if (state == 1) {
+    string = "7"
+  } else {
+    string += "7"
+  }
   var div = document.getElementById("output");
-  string += "7"
   div.innerHTML = string;
 }
 
 function d8() {
+  if (state == 1) {
+    string = "8"
+  } else {
+    string += "8"
+  }
   var div = document.getElementById("output");
-  string += "8"
   div.innerHTML = string;
 }
 
 function d9() {
+  if (state == 1) {
+    string = "9"
+  } else {
+    string += "9"
+  }
   var div = document.getElementById("output");
-  string += "9"
   div.innerHTML = string;
 }
 
@@ -58,4 +95,60 @@ function ac() {
   var div = document.getElementById("output");
   string = " "
   div.innerHTML = string;
+}
+
+function multi() {
+  var div = document.getElementById("output");
+  string += "*"
+  div.innerHTML = string;
+  state = 0;
+}
+
+function addi() {
+  var div = document.getElementById("output");
+  string += "+"
+  div.innerHTML = string;
+  state = 0;
+}
+
+function divi() {
+  var div = document.getElementById("output");
+  string += "/"
+  div.innerHTML = string;
+  state = 0;
+}
+
+function subtr() {
+  var div = document.getElementById("output");
+  string += "-"
+  div.innerHTML = string;
+  state = 0;
+}
+
+function equals() {
+  var div= document.getElementById("output");
+  string = eval(string);
+  div.innerHTML = Number(string);
+  state = 1;
+}
+
+function negative() {
+  var div= document.getElementById("output");
+  string = Number(string) * (-1);
+  div.innerHTML = string;
+  state = 0;
+}
+
+function parenthLeft() {
+ var div= document.getElementById("output");
+ string += "("
+ div.innerHTML = string;
+ state = 0;
+}
+
+function parenthRight() {
+ var div= document.getElementById("output");
+ string += ")"
+ div.innerHTML = string;
+ state = 0;
 }
